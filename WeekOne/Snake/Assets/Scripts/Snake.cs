b00104b34d0e4cc57af90f365cf88d886319fe3b
Kaarlo Kangas <kaarlo.kangas@gmail.com>
2021-06-08 13:05:56 +0300

    set up the scene and started working on the player characters movement

--- WeekOne/Snake/Assets/Scripts/Snake.cs
+++ WeekOne/Snake/Assets/Scripts/Snake.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
+    public SnakeSettings settings;
+    public GameObject player;
+    public Vector2 dir;
+    public float timeFromLastStep;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Snake");
+        dir = Vector2.right;
+        timeFromLastStep = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetInput();
+        if (timeFromLastStep >= settings.stepLength)
+        {
+            SnakeMove();
+            timeFromLastStep = 0;
+        }
+        else
+        {
+            timeFromLastStep += Time.deltaTime;
+        }
     }
+    void GetInput()
+    {
+        if (Input.GetKey(KeyCode.A))
+            dir = Vector2.left;
+        if (Input.GetKey(KeyCode.D))
+            dir = Vector2.right;
+        if (Input.GetKey(KeyCode.W))
+            dir = Vector2.up;
+        if (Input.GetKey(KeyCode.S))
+            dir = Vector2.down;
+    }
+    void SnakeMove()
+    {
+        player.transform.position = new Vector3(
+            Mathf.Clamp(player.transform.position.x + dir.x, -10, 9),
+            Mathf.Clamp(player.transform.position.y + dir.y, -5, 4),
+            0
+            );
+    }
+}
+[System.Serializable]
+public struct SnakeSettings
+{
+    public float stepLength;
+    public float pylly;
 }
