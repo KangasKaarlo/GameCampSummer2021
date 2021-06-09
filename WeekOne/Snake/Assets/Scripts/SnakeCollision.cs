bc02aac9f00a473446aa1c853fc4bdc94561ae80
Kaarlo Kangas <kaarlo.kangas@gmail.com>
2021-06-09 08:12:00 +0300

    fixed the snake movement and growth. Messed around with the apple texture

--- WeekOne/Snake/Assets/Scripts/SnakeCollision.cs
+++ WeekOne/Snake/Assets/Scripts/SnakeCollision.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class SnakeCollision : MonoBehaviour
 {
     public Snake snake;
-    
+    public Texture2D myTexture;
     private void Start()
     {
         snake = Camera.main.GetComponent<Snake>();
+          
     }
-private void OnTriggerEnter2D(Collider2D apple) {
-    Debug.Log("hit");
-    //snake.GrowSnake();
-    snake.MoveFood();        
-}
-    
+    private void OnTriggerEnter2D(Collider2D apple) {
+        Debug.Log("hit");
+        snake.GrowSnake();
+        snake.MoveFood();
+
+    }
 }
