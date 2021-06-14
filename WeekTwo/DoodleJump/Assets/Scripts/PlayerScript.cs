54f18b2d0503b6b8638fb203f04936a21c3474ce
Ville Karilainen <ville.karilainen@gmail.com>
2021-06-14 22:59:50 +0300

    collider changes

--- WeekTwo/DoodleJump/Assets/Scripts/PlayerScript.cs
+++ WeekTwo/DoodleJump/Assets/Scripts/PlayerScript.cs
@@ -39,7 +39,7 @@ public class PlayerScript : MonoBehaviour
     private bool IsGrounded()
     {
         RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
-        Debug.Log(raycastHit2d.collider);
+        //Debug.Log(raycastHit2d.collider);
         return raycastHit2d.collider != null;
     }
 }
