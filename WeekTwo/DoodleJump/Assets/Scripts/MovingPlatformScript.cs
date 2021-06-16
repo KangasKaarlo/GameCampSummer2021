5067788840ea91a5a0924d775f2b5e1e52ecdb24
Ville Karilainen <ville.karilainen@gmail.com>
2021-06-16 20:24:23 +0300

    Added prefab for moving platforms

---
+++ WeekTwo/DoodleJump/Assets/Scripts/MovingPlatformScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformScript : MonoBehaviour
+{
+    private GameObject player;
+    bool moveRight = true;
+    float moveSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >3f)
+        {
+            moveRight = false;
+        }
+        if (transform.position.x < -3f)
+        {
+            moveRight = true;
+        }
+        if (moveRight) {
+            transform.position = new Vector2(transform.position.x +moveSpeed*Time.deltaTime, transform.position.y );
+        }
+        else {
+            transform.position = new Vector2(transform.position.x -moveSpeed*Time.deltaTime, transform.position.y );
+        }
+    }
+}
