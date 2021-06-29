fd9a308b02e1855a104fc86e69971c35dcecc247
Ville Karilainen <ville.karilainen@gmail.com>
2021-06-29 12:56:00 +0300

    Sniper enemy fixed

--- WeekThree/TheRealDeal/Assets/Scripts/Enemy/Sniper.cs
+++ WeekThree/TheRealDeal/Assets/Scripts/Enemy/Sniper.cs
@@ -5,51 +5,52 @@ using UnityEngine;
 public class Sniper : Enemy
 {
     public float deltatime;
-    public bool burst;
-    public float ArrayList ;
+    public bool burst = true;
+    public float timer = 0f;
+    public float fireRate2 = 1f;
     public override void Move()
     {
+        deltatime = Time.deltaTime;
+        
+        if (timer <= fireRate2)
+        {
+            burst = true;
+            
+        }
+        else
+        {
+            
+            burst = false;
+        }
+        if (timer >= 3f)
+        {
+            timer = 0;
+        }
+        timer += deltatime;
 
     }
 
     override public void Shoot(float[] angles)
     {
-        deltatime = main.deltatime;
-        burst = true;
-        
-        if (timeFromLastShot > 2f)
-        {    
-            burst = true;
-            timeFromLastShot += deltatime;
-        }
-        else 
+        for (int i = 0; i < angles.Length; i++)
         {
-            burst = false;
-            
-            
-        } 
-        if (burst == true)
-        {
-            for (int i = 0; i < angles.Length; i++)
-            {        
-            
             GameObject newBullet = Instantiate(bullet, new Vector3(this.transform.position.x - 1f, this.transform.position.y, 0), Quaternion.identity);
             newBullet.GetComponent<Bullet>().playerBullet = false;
             newBullet.GetComponent<Bullet>().bulletBaseSpeed = -newBullet.GetComponent<Bullet>().bulletBaseSpeed;
             newBullet.GetComponent<Bullet>().angle = angles[i];
             Debug.Log(angles[i]);
-            timeFromLastShot = 0;
 
-            }
-            
-            
         }
     }
     public override float[] ShootingPattern()
     {
-        
-        //return new float[] { AngleBetweenVector2(new Vector2(this.transform.position.x, this.transform.position.y), new Vector2(player.transform.position.x, player.transform.position.y)) };
+        if (burst == true){
             return new float[] { AngleInDeg(this.transform.position, player.transform.position) };
+        }
+        //return new float[] { AngleBetweenVector2(new Vector2(this.transform.position.x, this.transform.position.y), new Vector2(player.transform.position.x, player.transform.position.y)) };
+        else {
+            return new float[] {  };
+        }  
 
 
         
